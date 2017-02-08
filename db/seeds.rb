a791fd006a25cb8431699892968c6f80f7e6dfeb
Thomas Sertorio <thomas.sertorio@gmail.com>
2017-02-08 18:52:34 +0100

    Yelp MVP with reviews in show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:        "Indian"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    category:        "Italian"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:        "Chinese"
+  },
+  {
+    name:         "Alicheur",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:        "Thai"
+  },
+  {
+    name:         "Subway",
+    address:      "4 rue Blainville 75005 Paris",
+    category:        "Fastfood"
+  },
+  {
+    name:         "Mamaroma",
+    address:      "route de Suresnes 75016 Paris",
+    category:        "Pizza"
+  }]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
+
+Review.create!(content: "That was awesome", rating: 3, restaurant: Restaurant.find_by(name: "Subway"))
+Review.create!(content: "That was horrible", rating: 3, restaurant: Restaurant.find_by(name: "Mamaroma"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
